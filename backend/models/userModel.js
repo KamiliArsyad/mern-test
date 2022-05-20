c6b97c0107889be0648fd19a2dcba0dd8e28688c
KamiliArsyad <arsyad.ik@gmail.com>
2022-05-20 21:22:44 +0800

    Added new user model

---
+++ backend/models/userModel.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose');
+
+const userSchema = mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "name is required."]
+    },
+    email: {
+        type: String,
+        required: [true, "email is required."],
+        unique: true
+    },
+    password: {
+        type: String,
+        required: [true, "Password is required."]
+    }
+},
+{
+    timestamps: true
+});
+
+module.exports = mongoose.model('User', userSchema);
